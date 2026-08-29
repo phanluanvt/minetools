2f2ba0a7ccc29dd805c6fbb96fea4fd00359282e
phanluanvt <phanluanvt@gmail.com>
2026-08-29 10:41:16 -0700

    Build shared Minecraft data and core P0 tools

---
+++ data/minecraft.js
@@ -0,0 +1,5 @@
+export const editions=["Java","Bedrock"];
+export const javaVersions=["1.21.x","1.20.x","1.19.x"];
+export const bedrockVersions=["1.21.x","1.20.x"];
+export const entities=["zombie","skeleton","creeper","spider","cave_spider","enderman","villager","iron_golem","warden","piglin","witch","blaze","ghast","slime","magma_cube","drowned","husk","stray","pillager","ravager","cow","pig","sheep","chicken","wolf","cat"];
+export const equipmentItems=["","wooden_sword","stone_sword","iron_sword","diamond_sword","netherite_sword","bow","crossbow","shield","leather_helmet","iron_helmet","diamond_helmet","netherite_helmet","leather_chestplate","iron_chestplate","diamond_chestplate","netherite_chestplate"];
